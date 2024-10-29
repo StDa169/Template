cf978135a69a4f6bdfff042248d4b220a7fda3ad
root <root@W9901469.paderborn.connext.de>
2024-10-29 14:34:07 +0100

    10/29/24 14:34:07

--- buildToPDF.sh
+++ buildToPDF.sh
@@ -14,14 +14,16 @@ zip -r ./Quellen.zip ./quellen/*
 find . -type f -name 'Thesis*' ! -name '*.pdf' ! -name '*.tex' -exec rm -f {} +
 
 # ==all.aux Files
-rm ./chapter/Anhang.aux
-rm ./chapter/Durchfuehrung.aux
-rm ./chapter/Ehrenwoertliche_Erklaerung.aux
-rm ./chapter/Einleitung.aux
-rm ./chapter/Grundlagen.aux
-rm ./chapter/Quellenverzeichnis.aux
-rm ./chapter/Sperrvermerk.aux
-rm ./chapter/Zusammenfassung.aux
+cd chapter
+rm Anhang.aux
+rm Durchfuehrung.aux
+rm Ehrenwoertliche_Erklaerung.aux
+rm Einleitung.aux
+rm Grundlagen.aux
+rm Quellenverzeichnis.aux
+rm Sperrvermerk.aux
+rm Zusammenfassung.aux
+cd ..
 
 # Auto Git-Commit
 git add .
