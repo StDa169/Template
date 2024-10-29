35f9a106399b69185f1cd3a1e7cab10a7e64b291
Stefan <std@connext.de>
2024-10-29 14:49:07 +0100

    10/29/24 14:49:07

--- buildToPDF.sh
+++ buildToPDF.sh
@@ -11,16 +11,7 @@ pdflatex ./Thesis
 # ==all Thesis files, but not .tex
 # find . -type f -name 'Thesis*' ! -name '*.pdf' ! -name '*.tex' -exec rm -f {} +
 # ==all.aux Files
-cd 'chapter'
-rm Anhang.aux
-rm Durchfuehrung.aux
-rm Ehrenwoertliche_Erklaerung.aux
-rm Einleitung.aux
-rm Grundlagen.aux
-rm Quellenverzeichnis.aux
-rm Sperrvermerk.aux
-rm *.aux
-cd ..
+rm ./chapter/*.aux
 # Auto Git-Commit
 git add .
 git commit -am "$(date +"%D %T")"
